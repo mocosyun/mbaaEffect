7f902cf76b2402e13d2983cbb2745095a3edc89c
syunsuke takahshi <syunsuke231@gmail.com>
2016-08-29 10:05:05 +0900

    エフェクト追加

---
+++ src/app.js
@@ -0,0 +1,108 @@
+var size;
+var num = 0;
+var skillnum = [380, 380, 380];
+var suu = 0;
+var gameScene = cc.Scene.extend({
+  onEnter: function() {
+    this._super();
+    var layer0 = new fieldLayer();
+    var layer1 = new gameLayer();
+    var layer2 = new charaLayer();
+    var layer3 = new particleLayer();
+    this.addChild(layer0);
+    this.addChild(layer1);
+    this.addChild(layer2);
+    this.addChild(layer3);
+
+  }
+});
+
+var gameLayer = cc.Layer.extend({
+  sprite: null,
+  ctor: function() {
+    this._super();
+    size = cc.winSize;
+    return true;
+  },
+
+});
+
+var fieldLayer = cc.Layer.extend({
+  ctor: function() {
+    this._super();
+
+    var size = cc.director.getWinSize();
+
+    var sprite = cc.Sprite.create(res.ss_BattleScene_bg1);
+    sprite.setPosition(size.width / 2, size.height / 2);
+    sprite.setScale(0.8);
+    this.addChild(sprite, 0);
+  }
+});
+
+var charaLayer = cc.Layer.extend({
+  ctor: function() {
+    this._super();
+    var size = cc.director.getWinSize();
+
+    //火属性のキャラクター
+    var sprite10 = cc.Sprite.create(res.chara_princessselect_10);
+    sprite10.setPosition(size.width * 0.3, size.height * 0.3);
+    sprite10.setScale(0.8);
+    this.addChild(sprite10, 0);
+
+
+    var sprite11 = cc.Sprite.create(res.chara_enemy_4);
+    sprite11.setPosition(size.width * 0.7, size.height * 0.4);
+    sprite11.setScale(0.8);
+    this.addChild(sprite11, 0);
+  }
+});
+
+//パーティクル用のレイヤー
+var particleLayer = cc.Layer.extend({
+  skillSelect: 0,
+  skillCnt: 1,
+
+  ctor: function() {
+    this._super();
+    size = cc.winSize;
+    this.scheduleUpdate();
+    return true;
+  },
+  update: function(_dt) {
+    if (this.skillCnt == 1) {
+
+     this.skillParticle(this.skillSelect);
+    }
+    if ((this.skillCnt % skillnum[suu]) == 0) {
+      this.skillCnt = 0;
+
+      this.removeAllChildren();
+      this.skillSelect++;
+      suu++;
+      this.skillSelect = this.skillSelect % 3;
+      if(suu > 2) suu = 0;
+
+    }
+    //フレームをカウントする
+    this.skillCnt++;
+  },
+//属性とスキルレベルと座標を与えてパーティクルを生成する関数
+  skillParticle: function(attrib) {
+    var skillName = ["laser", "fire", "flame"];
+    var x = [400, 400, 500, 700, 630, 630, 630, 650, 650, 619];
+    var y = [140, 140, 90, 70 , 125, 145, 165, 100, 120, 339];
+    var num2 = [4, 5, 4];
+    for(var i = 1; i < num2[attrib]; i++){
+      var sName = "res." + skillName[attrib] + "_particl" + i;
+      var tempParticle = new cc.ParticleSystem(eval(sName));
+      tempParticle.setPosition(x[num], y[num]);
+      num++;
+      if(num > 9) num = 0;
+      tempParticle.setDuration(5);
+      this.addChild(tempParticle, 20);
+      tempParticle.setAutoRemoveOnFinish(true);
+    }
+  },
+});
---
+++ src/resource.js
@@ -0,0 +1,33 @@
+var res = {
+  HealTexture_png: "res/Heal_texture.png",
+  DotTexture_png: "res/Dot_texture.png",
+  particle_texture: "res/Dot_texture.png",
+  chara_princessselect_10: "res/char/chara_princessselect_10.png",
+  chara_princessselect_11: "res/char/chara_princessselect_11.png",
+  chara_princessselect_12: "res/char/chara_princessselect_12.png",
+  ss_BattleScene_bg1: "res/field/ss_BattleScene_bg1.jpg",
+  ss_BattleScene_bg2: "res/field/ss_BattleScene_bg2.jpg",
+  ss_BattleScene_bg3: "res/field/ss_BattleScene_bg3.jpg",
+  chara_enemy_1: "res/char/chara_enemy_1.png",
+  chara_enemy_2: "res/char/chara_enemy_2.png",
+  chara_enemy_3: "res/char/chara_enemy_3.png",
+  chara_enemy_4: "res/char/chara_enemy_4.png",
+  chara_enemy_5: "res/char/chara_enemy_5.png",
+  chara_enemy_6: "res/char/chara_enemy_6.png",
+
+  laser_particl1: "res/effect/laser1_particle.plist",
+  laser_particl2: "res/effect/laser2_particle.plist",
+  fire_particl1: "res/effect/fire1_particle.plist",
+  fire_particl2: "res/effect/fire2_particle.plist",
+  fire_particl3: "res/effect/fire3_particle.plist",
+  fire_particl4: "res/effect/fire4_particle.plist",
+  fire_particl5: "res/effect/fire5_particle.plist",
+  flame_particl1: "res/effect/flame1_particle.plist",
+  flame_particl2: "res/effect/flame2_particle.plist"
+
+};
+
+var g_resources = [];
+for (var i in res) {
+  g_resources.push(res[i]);
+}
